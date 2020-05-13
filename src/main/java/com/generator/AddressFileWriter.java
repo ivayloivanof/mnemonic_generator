ec107c679f72144d9de5dc7bb6c71e1449e30fe7
Ivaylo Ivanov <ivaylo.ivanof@gmail.com>
2020-05-13 07:11:11 +0300

    create file writer

---
+++ src/main/java/com/generator/AddressFileWriter.java
@@ -0,0 +1,23 @@
+package com.generator;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+class AddressFileWriter {
+
+	public static void writeFile(List<String> string, String fileName) {
+        BufferedWriter writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName, true));
+            for (String str : string) {
+                writer.append(str + System.lineSeparator());
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
